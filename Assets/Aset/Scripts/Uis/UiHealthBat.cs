9de7ea629fe8edf2addacda897c35ff5ef76d5c3
Potter3rd <fiksmane1@gmail.com>
2026-04-15 13:50:32 -0700

    fixewd things

--- Assets/Aset/Scripts/Uis/UiHealthBat.cs
+++ Assets/Aset/Scripts/Uis/UiHealthBat.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiHealthBat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //to start the slider for the health bar
+    public Slider slider;
+
+    private void SetMaxHealth(int health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetHealth(int health)
     {
-        
+        slider.value = health;
     }
 }
